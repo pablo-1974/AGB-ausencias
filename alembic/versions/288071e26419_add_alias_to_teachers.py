487e5b490268d02c0751e2dc032c77a00367e633
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-03-08 18:27:43 +0100

    Update 288071e26419_add_alias_to_teachers.py

--- alembic/versions/288071e26419_add_alias_to_teachers.py
+++ alembic/versions/288071e26419_add_alias_to_teachers.py
@@ -1,11 +1,19 @@
+"""add alias to teachers
+
+Revision ID: 288071e26419
+Revises: 840d22bcafac
+Create Date: 2026-03-08 17:11:56.296753
+
+"""
+from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = "288071e26419"          # <-- PON aquí el ID del archivo
-down_revision = "840d22bcafac"     # <-- tu migración inicial
-branch_labels = None
-depends_on = None
+revision: str = "288071e26419"
+down_revision: Union[str, None] = "840d22bcafac"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.add_column("teachers", sa.Column("alias", sa.String(length=120), nullable=True))
@@ -14,3 +22,4 @@ def upgrade() -> None:
 def downgrade() -> None:
     op.drop_index("ix_teachers_alias", table_name="teachers")
     op.drop_column("teachers", "alias")
+
