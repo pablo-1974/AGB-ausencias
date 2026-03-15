b53f70bbe1c7e8cc18e80304d20b3b115d8d758f
pablo-1974 <pceballosroa@educa.jcyl.es>
2026-03-15 23:30:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ from starlette.middleware.sessions import SessionMiddleware
 ########## PROVISIONAL ##############################
 import os
 print("DEBUG FILES:", os.listdir("."))
-print("SECRET_KEY IN RUNTIME:", settings.SECRET_KEY)
 ####################################################
 
 from config import settings
+print("SECRET_KEY IN RUNTIME:", settings.SECRET_KEY)   # ← YA FUNCIONA
+
 from auth import router as auth_router
 
 
